347a9f966272abb569e3e20e55b6e8b503958c18
Xiaocong (Rain) Yan <50472308+xcupsilon@users.noreply.github.com>
2022-05-17 17:52:33 -0400

    Sets up full-stack base

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <div className="flex justify-center w-screen h-screen">
+      <div className="flex items-center">
+        <h1>
+          Hello World
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default App;
